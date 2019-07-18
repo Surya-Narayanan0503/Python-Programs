17d863843203b5eb7617132910c6c08b619e7099
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2019-07-18 10:38:11 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,14 @@
 num = int(input())
- 
 temp = num
+num=a
+a=b
 reverse = 0
  
 while temp != 0:
 	reverse = (reverse * 10) + (temp % 10)
 	temp = temp // 10
  
-if num == reverse:
+if b == reverse:
 	print("yes")
 else:
 	print("no")
