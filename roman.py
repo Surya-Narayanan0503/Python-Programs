2fcdc31ed3c4cc068f2a23ef02a465673c0700e7
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2019-07-19 14:19:07 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,7 @@
+aa=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+bb=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+cc=str(input())
+for dd in range(20):
+  if(cc==aa[dd]):
+    print(bb[dd])
+    break
